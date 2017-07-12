4841349e092b1fac351498918d7e783db8c51ab3
Murray Berkowitz <murray.berkowitz93@gmail.com>
2017-07-11 21:51:25 -0400

    created middleware that logs all incoming request

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const chalk = require('chalk');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(function(req,res,next){
+    if(req.method == 'GET'){
+        console.log(chalk.bold.red(req.method, req.url));
+    }
+    else if(req.method == 'POST'){
+        console.log(chalk.bold.yellow(req.method, req.url));
+    }
+    else if(req.method == 'PUT'){
+        console.log(chalk.bold.green(req.method, req.url));
+    }
+    else {
+        console.log(chalk.bold.blue(req.method, req.url));
+    }
+    next();
+})
+
+app.get('/', function(req,res,next){
+    res.send('Hello!!!!');
+})
+
+app.get('/news', function(req,res,next){
+    res.send('news biatch');
+})
+
+app.listen(port, function(){
+    console.log(`Server running on ${port}...`);
+})
